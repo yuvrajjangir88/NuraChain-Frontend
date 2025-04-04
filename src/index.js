f849ea352fd2245fd1e2ee55362737d563632d8f
yuvrajjangir <yuvijangir78@gmail.com>
2025-04-04 12:08:54 +0530

    url changed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import App from './App';
 import axios from 'axios';
 
 // Configure axios defaults
-axios.defaults.baseURL = 'https://nurachain-backend-e3562vnzj-yuvrajjangirs-projects.vercel.app/';
+axios.defaults.baseURL = 'https://nurachain-backend-jxu4fbh31-yuvrajjangirs-projects.vercel.app';
+// Configure axios for CORS
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
